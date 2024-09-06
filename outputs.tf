43640da6cb0d981634da489adab6080471250024
greyhoundforty <ryantiffany@fastmail.com>
2024-09-06 08:32:35 -0500

    Added updated diagram and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,23 @@
+output "ts_router_ip" {
+  value = module.ts_router.compute_instance_ip
+}
+
 output "dev_node_ip" {
   value = module.dev_compute.compute_instance_ip
 }
 
 output "prod_node_ip" {
   value = module.prod_compute.compute_instance_ip
+}
+
+output "hub_vpc_subnet" {
+  value = module.hub_vpc.vpc_subnet_cidr
+}
+
+output "prod_vpc_subnet" {
+  value = module.prod_vpc.vpc_subnet_cidr
+}
+
+output "dev_vpc_subnet" {
+  value = module.dev_vpc.vpc_subnet_cidr
 }
